c8de02deb64a040933bac143c5346e32bd0a048d
habtegiorgiskassaye21 <habtegiorgiskassaye21@gmail.com>
2026-06-15 13:38:47 +0300

    Delete javascript.js

--- javascript.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-div");
-    if(menu.style.display==="block"){
-        menu.style.display="none"
-    }else{
-        menu.style.display="block"
-    }
-}
